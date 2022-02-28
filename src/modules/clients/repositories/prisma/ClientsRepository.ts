749ad8f5c2881d353e6754aa46ce4d215f28bf00
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2022-02-28 18:01:24 -0300

    feat: adicionando classe de repositorio para futura implementação

---
+++ src/modules/clients/repositories/prisma/ClientsRepository.ts
@@ -0,0 +1,28 @@
+import { Client } from "@prisma/client";
+import { prisma } from "../../../../database/prismaClient";
+import { ICreateClientDTO } from "../../dtos/ICreateClientDTO";
+import { IClientsRepository } from "../IClientsRepository";
+
+class ClientsRepository implements IClientsRepository {
+    async create({ username, password }: ICreateClientDTO): Promise<Client> {
+        const client = await prisma.client.create({
+            data: {
+                username,
+                password,
+            }
+        });
+
+        return client;
+    }
+
+    async findByUserName(username: string): Promise<Client | null> {
+        const client = await prisma.client.findUnique({
+            where: {
+                username
+            }
+        });
+
+        return client;
+    }
+
+}
