529e60c1f7b3eafad4338b3b84a192b2c34448da
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2022-05-06 10:59:58 -0300

    feat: adicionando validação no CreateDelivery

--- src/modules/delivery/useCases/createDelivery/CreateDeliveryUseCase.ts
+++ src/modules/delivery/useCases/createDelivery/CreateDeliveryUseCase.ts
@@ -1,5 +1,6 @@
 import { Delivery } from "@prisma/client";
 import { prisma } from "../../../../database/prismaClient";
+import { AppError } from "../../../../shared/errors/AppError";
 
 
 interface ICreateDeliveryDTO {
@@ -8,6 +9,10 @@ interface ICreateDeliveryDTO {
 }
 export class CreateDeliveryUseCase {
     async execute({item_name, id_client}: ICreateDeliveryDTO): Promise<Delivery> {
+        if (!item_name || !id_client) {
+            throw new AppError("Please, review the request");
+        }
+
         const delivery = await prisma.delivery.create({
             data: {
                 item_name,
