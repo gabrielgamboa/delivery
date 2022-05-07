414db4a97c5f43a84fad0d19b1435dbb6f58f222
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2022-05-07 20:52:29 -0300

    feat: adicionando docker

--- src/modules/clients/useCases/createClient/CreateClientUseCase.ts
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -6,26 +6,26 @@ import { ICreateClientDTO } from "../../dtos/ICreateClientDTO";
 
 
 export class CreateClientUseCase {
-    async execute({username, password}: ICreateClientDTO): Promise<Client> {
-        const clientAlreadyExists = await prisma.client.findUnique({
-            where: {
-                username
-            }
-        });
+  async execute({ username, password }: ICreateClientDTO): Promise<Client> {
+    const clientAlreadyExists = await prisma.client.findUnique({
+      where: {
+        username
+      }
+   });
 
-        if (clientAlreadyExists) {
-            throw new AppError("Client already exists.");
-        }
-
-        const passwordHash = await hash(password, 8);
-
-        const client = await prisma.client.create({
-            data: {
-                username,
-                password: passwordHash,
-            }
-        });
-
-        return client;
+    if (clientAlreadyExists) {
+      throw new AppError("Client already exists.");
     }
+
+    const passwordHash = await hash(password, 8);
+
+    const client = await prisma.client.create({
+      data: {
+        username,
+        password: passwordHash,
+      }
+    });
+
+    return client;
+  }
 }
